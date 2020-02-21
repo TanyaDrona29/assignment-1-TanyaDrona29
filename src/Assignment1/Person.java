2c191cae4023df5c054e2b34361d7eaa021a6944
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-02-21 17:45:33 +0530

    creating getter and setters

--- src/Assignment1/Person.java
+++ src/Assignment1/Person.java
@@ -12,4 +12,36 @@ public class Person {
         this.numberList =  numberList;
         this.emailAddress = emailAddress;
     }
+
+    public String[] getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String[] emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String[] getNumberList() {
+        return numberList;
+    }
+
+    public void setNumberList(String[] numberList) {
+        this.numberList = numberList;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
