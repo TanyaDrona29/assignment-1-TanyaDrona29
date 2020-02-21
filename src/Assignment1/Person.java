aa1f813d32e2e703743c1fc7a5358e229e7e9c5d
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-02-21 15:18:18 +0530

    creating parameters

---
+++ src/Assignment1/Person.java
@@ -0,0 +1,8 @@
+package Assignment1;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String[] numberList = new String[10];
+    private String[] emailAddress = new String[10];
+}
