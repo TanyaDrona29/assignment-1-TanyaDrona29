05a76ab04d49375be0b75321f510098830b1153e
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-02-22 16:34:58 +0530

    creating getter and setters for all defined parameters

--- src/Assignment1/PersonDetails/Person.java
+++ src/Assignment1/PersonDetails/Person.java
@@ -6,4 +6,35 @@ public class Person {
     private String[] numberList;
     private String emailAddress = "";
 
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String[] getNumberList() {
+        return numberList;
+    }
+
+    public void setNumberList(String[] numberList) {
+        this.numberList = numberList;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
