229675d9c628bb79a194aa0797edffa294cfe757
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-02-22 16:33:33 +0530

    defining paramters

---
+++ src/Assignment1/PersonDetails/Person.java
@@ -0,0 +1,9 @@
+package Assignment1.PersonDetails;
+
+public class Person {
+    private String firstName = "";
+    private String lastName = "";
+    private String[] numberList;
+    private String emailAddress = "";
+
+}
