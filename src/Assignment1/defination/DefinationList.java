087ff2dfbfe2cbe540c80e518d26f06cb50dbda9
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2020-02-22 17:22:39 +0530

    Node() method defination

--- src/Assignment1/defination/DefinationList.java
+++ src/Assignment1/defination/DefinationList.java
@@ -11,6 +11,13 @@ public class DefinationList<Person> implements LinkedListADT<Person> {
     private Node<Person> head = null;
     private int size = 0;
 
+    public Node<Person> getNode(int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.getNext();
+        }
+        return temp;
+    }
     private static class Node<Person>{
         private Node next = null;
         private Person data = null;
